bc24e4b5b2f903dc344e03cb51f7a0827c338b78
davidfpc <davidcardopinto@hotmail.com>
2013-11-28 21:35:20 +0000

    Features are now commented (and other stuff)

--- src/model/Features.java
+++ src/model/Features.java
@@ -49,19 +49,38 @@ public interface Features extends Remote{
     /**
      *
      * @param data Idea info
+     *      <p>topicID (lots of them)</p>
+            <p>size - 4 = IdeaName </p>
+            <p>size - 3 = IdeaDescription</p>
+            <p>size - 2 = DEIcoins   </p>
+            <p>size - 1 = The ID of the parent Idea </p>
      * @param id_user Id of the user
-     * @param ischild
+     * @param ischild If the idea is a reply
      * @return
      * @throws RemoteException
      */
     public int newIdea(ArrayList<String> data, int id_user, boolean ischild) throws RemoteException;
+
+    /**
+     *
+     * @param data a arrayList of strings containing only the Topic Title
+     * @return the ID of the Topic or -1in case of error
+     * @throws RemoteException
+     */
     public int newTopics(ArrayList<String> data) throws RemoteException;
+
     public ArrayList<String> getTopics() throws RemoteException;
     public ArrayList<String> getIdeas(String Topic) throws RemoteException;
     public ArrayList<String> getIdeaDetails(String idea) throws RemoteException;
     public ArrayList<String> getSharesInfo(String idea)throws RemoteException;
     public ArrayList<String> buyShares(String idea, int percentage, int ID_User, int sellingPrice, int maxprice, String timeStamp) throws RemoteException;
     public ArrayList<String> getTransactions(int ID_user,boolean limited) throws RemoteException;
+    /**
+     * UPDATES THE LAST LOGIN OF A USER
+     * @param ID_User
+     * @return 1 in case of sucess, -1 in case of failure
+     * @throws RemoteException
+     */
     public int updateLastLogin(int ID_User) throws RemoteException ;
     public ArrayList<String> getuserIdeas(int ID_User) throws RemoteException;
     public int deleteIdeas(String idea, int ID_User) throws RemoteException;
